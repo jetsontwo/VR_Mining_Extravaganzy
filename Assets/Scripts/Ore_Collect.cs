2f6d2e1392dd12ff4c887cd345c95f605b21c941
jetsontwo <jetsontwo@gmail.com>
2017-05-27 16:34:53 -0700

    Added a Cart That Holds Ore
    Created a cart that holds ore that can be brought back to the players
    base.

---
+++ Assets/Scripts/Ore_Collect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ore_Collect : MonoBehaviour {
+
+    private GameObject[] ores;
+    private int ore_count = 0;
+
+    void Start()
+    {
+        ores = new GameObject[20];
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Ore" && ore_count < ores.Length - 1)
+        {
+            ores[ore_count++] = c.gameObject;
+            c.gameObject.SetActive(false);
+        }
+    }
+}
