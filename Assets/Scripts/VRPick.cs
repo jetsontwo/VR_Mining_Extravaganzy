c18873447e6c2fc60ef1d5745e270f4012e61154
jetsontwo <jetsontwo@gmail.com>
2017-05-27 15:38:34 -0700

    Added Interactive Pick and Fixed it to Position
    Added a pick that can be picked up and then it slots into position on
    the hand.

---
+++ Assets/Scripts/VRPick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class VRPick : VRTK_InteractableObject
+{
+
+    public override void StartUsing(GameObject currentUsingObject)
+    {
+        base.StartUsing(currentUsingObject);
+        print("Hello");
+    }
+
+}
