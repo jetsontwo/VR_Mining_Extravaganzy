b13dcfc5dba34b51bb2ca78990799e19d1b57ecc
jetsontwo <jetsontwo@gmail.com>
2017-05-27 18:12:37 -0700

    Player Now Teleports to Other Location When Clicking Button
    Has the blink feature to reduce nausea.

--- Assets/Scripts/Home_Button.cs
+++ Assets/Scripts/Home_Button.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-public class Home_Button : MonoBehaviour {
+public class Home_Button : VRTK_DestinationMarker
+{ 
+    public Transform destination;
+    private bool lastUsePressedState = false;
 
-    void OnTriggerEnter(Collider c)
+    private void OnTriggerStay(Collider collider)
     {
-        print("yo");
-        print(c.tag);
+        var controller = (collider.GetComponent<VRTK_ControllerEvents>() ? collider.GetComponent<VRTK_ControllerEvents>() : collider.GetComponentInParent<VRTK_ControllerEvents>());
+        if (controller)
+        {
+            if (lastUsePressedState == true && !controller.usePressed)
+            {
+                var distance = Vector3.Distance(transform.position, destination.position);
+                var controllerIndex = VRTK_DeviceFinder.GetControllerIndex(controller.gameObject);
+                OnDestinationMarkerSet(SetDestinationMarkerEvent(distance, destination, new RaycastHit(), destination.position, controllerIndex));
+            }
+            lastUsePressedState = controller.usePressed;
+        }
     }
 }
